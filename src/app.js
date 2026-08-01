009da9480296fa3dd1bc5a8e1c5e8f6d996847ee
Palachev <99887305+Palachev@users.noreply.github.com>
2026-08-01 14:06:19 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,8 +33,8 @@ app.listen(port, () => {
 //  First, install the dependencies listed in package.json:
 //   npm install
 //
-// Then, start the API server with:
+// Then, start the API server with:ю
 //   node app.js
 //
 
-// Trigger CI/CD pipeline..
+// Trigger CI/CD pipeline.....
